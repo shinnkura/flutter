95015d06e59c849f222284e4f0b5da9e3b6999c8
shinnkura <n.shinzo1010@icloud.com>
2025-02-16 13:08:21 +0900

    singout設定

--- sns_app/lib/widgets/header.dart
+++ sns_app/lib/widgets/header.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sns_app/modules/auth/auth.repository.dart';
+import 'package:sns_app/modules/auth/current_user.store.dart';
 
-class Header extends StatelessWidget {
+class Header extends ConsumerWidget {
   const Header({
     super.key,
   });
 
+  void _handleMenuSelected(WidgetRef ref, int val) async {
+    if (val == 1) {
+      await AuthRepository().signout();
+      ref.watch(currentUserProvider.notifier).setCurrentUser(null);
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserProvider);
+    if (currentUser == null) return const SizedBox.shrink();
+
     return AppBar(
       title: const Text('SNS APP', style: TextStyle(color: Colors.white)),
       actions: [
@@ -16,8 +29,8 @@ class Header extends StatelessWidget {
             PopupMenuItem<int>(
               value: 0,
               child: ListTile(
-                title: Text("user name"),
-                subtitle: Text("user email"),
+                title: Text(currentUser.userMetadata!['name']),
+                subtitle: Text(currentUser.email!),
               ),
             ),
             const PopupMenuItem<int>(
@@ -31,6 +44,7 @@ class Header extends StatelessWidget {
                   ),
                 ))
           ],
+          onSelected: (val) => _handleMenuSelected(ref, val),
         ),
       ],
     );
